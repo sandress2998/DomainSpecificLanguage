ce9dff8903383bd0affdbd3ae4603badd367b4d9
sandress2998 <sandress2000@mail.ru>
2024-10-06 13:46:18 +0300

    Create project

---
+++ src/Main.kt
@@ -0,0 +1,128 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+data class Response (
+    val code: Int,
+    val body: String?
+)
+
+class Client {
+    fun perform(code: Int, body: String?) = ResponseActions(code, body)
+}
+
+class ResponseActions(code: Int, body: String?) {
+    val response: Response = Response(code, body)
+
+    fun andDo(responseFunction: (Response) -> Unit): ResponseActions {
+        responseFunction(response)
+        return this
+    }
+
+    fun andExpect(responseMatcher: () -> Unit) : ResponseActions {
+        return this.apply {
+            responseMatcher()
+        }
+    }
+
+    fun status(responseFunction: ResponseActions.StatusResponseActions.() -> Unit) {
+        this.StatusResponseActions().responseFunction()
+    }
+
+    fun body(responseFunction: ResponseActions.BodyResponseActions.() -> Unit) {
+        this.BodyResponseActions().responseFunction()
+    }
+
+    inner class StatusResponseActions {
+        fun isOk() {
+            if (response.code != 200) throw StatusResponseMatchersException()
+        } // если статус не 200, то выбросить исключение
+
+        fun isBadRequest() {
+            if (response.code != 400) throw StatusResponseMatchersException()
+        }// если статус не 400, то выбросить исключение
+
+        fun isInternalServerError() {
+            if (response.code != 500) throw StatusResponseMatchersException()
+        } // если статус не 500, то выбросить
+    }
+
+    inner class BodyResponseActions() {
+        fun isNull() {
+            if (!response.body.isNullOrEmpty()) throw BodyResponseMatchersException()
+        } // если тело не пустое, то выбросить исключение
+
+        fun isNotNull() {
+            if (response.body.isNullOrEmpty()) throw BodyResponseMatchersException()
+        }
+    }
+}
+
+open class ResponseMatchersException: Exception() {}
+class StatusResponseMatchersException: ResponseMatchersException() {}
+class BodyResponseMatchersException: ResponseMatchersException() {}
+
+
+fun mock() {
+    println("This shouldn't display")
+}
+fun main() {
+    val mockClient = Client()
+    val response: Response = mockClient.perform(200, "OK").apply {
+        andExpect {
+            status {
+                isOk()
+            }
+            body {
+                isNotNull()
+            }
+        }
+        andDo { response ->
+            println(response)
+        }
+    }.response
+}
+
+
+
+
+
+
+
+
+
+
+
+/*
+fun andExpect(responseMatchers: (statusResponseMatchers: () -> Unit, bodyResponseMatchers: () -> Unit) -> Unit) : ResponseActions {
+    return this
+}
+
+fun status(responseActions: ResponseActions.StatusResponseActions.() -> Unit) {
+    this.StatusResponseActions().apply {
+        responseActions()
+    }
+}
+fun body(responseActions: ResponseActions.BodyResponseActions.() -> Unit) {
+    this.BodyResponseActions().apply {
+        responseActions()
+    }
+}
+
+fun ResponseActions.StatusResponseActions.status(responseAction: ResponseActions.StatusResponseActions.() -> Unit) {
+    responseAction()
+}
+
+fun ResponseActions.BodyResponseActions.body(responseAction: ResponseActions.BodyResponseActions.() -> Unit) {
+    responseAction()
+}
+
+fun status(responseFunction:  ResponseActions.StatusResponseActions.() -> Unit) { responseFunction() }
+fun body(responseFunction: ResponseActions.BodyResponseActions.() -> Unit) { this.responseFunction() }
+
+fun status(responseActions: ResponseActions, responseFunction: ResponseActions.StatusResponseActions.() -> Unit) {
+    responseActions.StatusResponseActions().responseFunction()
+}
+
+fun body(responseActions: ResponseActions, responseFunction: ResponseActions.BodyResponseActions.() -> Unit) {
+    responseActions.BodyResponseActions().responseFunction()
+}
+*/
